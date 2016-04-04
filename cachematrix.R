2f4bc028fa34814ecc85e025121564ff0d31c31e
Donald <donald_fern@hotmail.com>
2016-04-04 16:26:44 -0700

    Function to compute and cache Inverse of a given Matrix
    This makes use of the inbuilt SOLVE() function in R.
    Assumption is that the Matrix is invertible.
    Results are cached and repeatable, when the original Matrix stays
    unchanged.

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,48 @@
 ## functions do
 
 ## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) 
+{
+        invmat <- NULL
+        setmatrix <- function(y) 
+        {
+                x <<- y
+                invmat <<- NULL
+        }
+        getmatrix <- function() x
+        setinverse <- function(inverse) invmat <<- inverse
+        getinverse <- function() invmat
+        list(setmatrix = setmatrix, 
+             getmatrix = getmatrix,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) 
+{
         ## Return a matrix that is the inverse of 'x'
+        invmat <- x$getinverse()
+        if(!is.null(invmat)) 
+        {
+                message("getting previouly cached matrix inverse data...")
+                return(invmat)
+        }
+        message("computing fresh matrix inverse data...")
+        data <- x$getmatrix()
+        invmat <- solve(data) ## this is core inversion formula
+        x$setinverse(invmat)
+        invmat
 }
+
+## call usage for testing
+## df <- matrix (c(2,1,3,1),2,2)
+## dfi <- makeCacheMatrix(df)
+## dfi$getmatrix()
+## cacheSolve(dfi) 
+## dfi$getinverse()
+## dfi$getinverse()
+## expected_output <- matrix (c(-1,1,3,-2),2,2)
+
